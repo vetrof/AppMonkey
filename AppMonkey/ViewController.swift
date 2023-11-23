00ad740ed5caa084b28cbf05baf75c7b75efe256
Vitaly Vetrof <vetrof@gmail.com>
2023-11-23 16:02:44 +0600

    init

--- AppMonkey/ViewController.swift
+++ AppMonkey/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //экран БЫЛ загружен
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
 
-
+    //экран БУДЕТ показан
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    //экран БЫЛ показан
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // экран Будет скрыт
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    // экран БЫЛ скрыт
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
 }
 
